7aefacc3f91c5c77b99cc8d57648b4d2cf7a12d5
Noah Solomon <noahsolomon2003@gmail.com>
2024-01-19 16:28:20 -0500

    different title for app section

--- src/components/project/Projects.tsx
+++ src/components/project/Projects.tsx
@@ -86,7 +86,7 @@ const Projects = () => {
   return (
     <Section sectionName="apps" className="spacing lg:h-[110vh]">
       <article className="flex flex-col gap-24 w-full">
-        <Heading text="Most Popular Apps." />
+        <Heading text="Recent Apps and Tools." />
 
         <ul className="grid grid-cols-1 gap-8 max-w-2xl sm:grid-cols-2">
           {projects}
