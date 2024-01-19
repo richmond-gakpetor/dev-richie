9086978368a286574d70c1dc9797ceb63a780a84
Noah Solomon <noahsolomon2003@gmail.com>
2024-01-19 16:26:14 -0500

    Add project information and utility function

--- src/components/utils/projectInfo.tsx
+++ src/components/utils/projectInfo.tsx
@@ -8,35 +8,43 @@ export type URL = {
 type ProjectInfo = {
   key: string;
   name: string;
+  prevIcon: boolean;
+  description: string;
   icon: JSX.Element;
   img: string;
+  domain: string;
   stack: string[];
   source?: URL;
   website: URL;
   video: string;
   title: string;
+  emoji?: string;
   paragraphs: {
     [key: string]: string;
   };
+  progress: "Building" | "Shipped";
 };
 
 // Create a new Map instance
 const projectInfo = new Map<string, ProjectInfo>();
-const size = 30;
 
 // Add entries to the Map
 projectInfo.set("smartwtf", {
   key: "smartwtf",
   name: "Smart.wtf",
+  progress: "Building",
+  domain: "smart.wtf",
   icon: (
     <Image
-      width={48}
-      height={48}
-      className="my-3"
+      width={32}
+      height={32}
       src={"https://images.codefoli.com/smartwtf.png"}
       alt="smartwtf logo"
     />
   ) as JSX.Element,
+  prevIcon: true,
+  description:
+    "AI first learning platform designed to make the learning process fun, intuitive, and absurd.",
   img: "https://images.codefoli.com/smart.png",
   video: "https://images.codefoli.com/smartwtfdemo.mp4",
   stack: ["TypeScript", "Next.js", "tRPC", "Clerk", "MySQL", "Tailwind.css"],
@@ -52,6 +60,8 @@ projectInfo.set("smartwtf", {
 projectInfo.set("codefoli", {
   key: "codefoli",
   name: "Codefoli",
+  domain: "codefoli.com",
+  emoji: "☁️",
   icon: (
     <Image
       width={148}
@@ -61,9 +71,108 @@ projectInfo.set("codefoli", {
       alt="codefoli logo"
     />
   ),
+  description:
+    "Portfolio websites built and hosted for developers who have better things to do.",
+  prevIcon: false,
+  progress: "Shipped",
   img: "https://images.codefoli.com/codefolilogo.png",
   video: "https://images.codefoli.com/codefolivideo.mp4",
-  stack: ["AWS (19 Services)", "React", "TypeScript", "Tailwind.css"],
+  stack: ["AWS", "React", "TypeScript", "Tailwind.css"],
+  source: {
+    name: "noahgsolomon/codefoli",
+    href: "https://github.com/noahgsolomon/codefoli",
+  },
+  website: { name: "codefoli", href: "https://codefoli.com" },
+  title: "Codefoli",
+  paragraphs: {
+    "1": "Building portfolio websites for developers who have better things to do. Codefoli is a platform that allows developers to build and deploy a portfolio website in minutes. Whether you are coming in with your own domain, or would just like to deploy through a subdomain (e.g. elonmusk.codefoli.com), our infrastructure is efficient enough to offer this for $0!.",
+  },
+});
+
+projectInfo.set("codefolie", {
+  key: "codefolie",
+  name: "Codefoli",
+  domain: "codefoli.com",
+  icon: (
+    <Image
+      width={148}
+      height={148}
+      className="my-3 border border-gray-600 rounded-lg border-opacity-50"
+      src={"https://images.codefoli.com/codefoli.png"}
+      alt="codefoli logo"
+    />
+  ),
+  description:
+    "Portfolio websites built and hosted for developers who have better things to do.",
+  prevIcon: false,
+  emoji: "☁️",
+  progress: "Shipped",
+  img: "https://images.codefoli.com/codefolilogo.png",
+  video: "https://images.codefoli.com/codefolivideo.mp4",
+  stack: ["AWS", "React", "TypeScript", "Tailwind.css"],
+  source: {
+    name: "noahgsolomon/codefoli",
+    href: "https://github.com/noahgsolomon/codefoli",
+  },
+  website: { name: "codefoli", href: "https://codefoli.com" },
+  title: "Codefoli",
+  paragraphs: {
+    "1": "Building portfolio websites for developers who have better things to do. Codefoli is a platform that allows developers to build and deploy a portfolio website in minutes. Whether you are coming in with your own domain, or would just like to deploy through a subdomain (e.g. elonmusk.codefoli.com), our infrastructure is efficient enough to offer this for $0!.",
+  },
+});
+
+projectInfo.set("codefolie", {
+  key: "codefolie",
+  name: "Codefoli",
+  domain: "codefoli.com",
+  icon: (
+    <Image
+      width={148}
+      height={148}
+      className="my-3 border border-gray-600 rounded-lg border-opacity-50"
+      src={"https://images.codefoli.com/codefoli.png"}
+      alt="codefoli logo"
+    />
+  ),
+  description:
+    "Portfolio websites built and hosted for developers who have better things to do.",
+  prevIcon: false,
+  progress: "Shipped",
+  img: "https://images.codefoli.com/codefolilogo.png",
+  video: "https://images.codefoli.com/codefolivideo.mp4",
+  stack: ["AWS", "React", "TypeScript", "Tailwind.css"],
+  source: {
+    name: "noahgsolomon/codefoli",
+    href: "https://github.com/noahgsolomon/codefoli",
+  },
+  website: { name: "codefoli", href: "https://codefoli.com" },
+  title: "Codefoli",
+  paragraphs: {
+    "1": "Building portfolio websites for developers who have better things to do. Codefoli is a platform that allows developers to build and deploy a portfolio website in minutes. Whether you are coming in with your own domain, or would just like to deploy through a subdomain (e.g. elonmusk.codefoli.com), our infrastructure is efficient enough to offer this for $0!.",
+  },
+});
+
+projectInfo.set("codefole", {
+  key: "codefole",
+  name: "Codefoli",
+  domain: "codefoli.com",
+  emoji: "☁️",
+  icon: (
+    <Image
+      width={148}
+      height={148}
+      className="my-3 border border-gray-600 rounded-lg border-opacity-50"
+      src={"https://images.codefoli.com/codefoli.png"}
+      alt="codefoli logo"
+    />
+  ),
+  description:
+    "Portfolio websites built and hosted for developers who have better things to do.",
+  prevIcon: false,
+  progress: "Shipped",
+  img: "https://images.codefoli.com/codefolilogo.png",
+  video: "https://images.codefoli.com/codefolivideo.mp4",
+  stack: ["AWS", "React", "TypeScript", "Tailwind.css"],
   source: {
     name: "noahgsolomon/codefoli",
     href: "https://github.com/noahgsolomon/codefoli",
